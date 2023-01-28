3589daa69412c56703e6eed397a5da57ce55f223
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-28 03:09:18 +0000

    commit

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,27 +1,29 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "February 10th @ 12:00pm",
-    reminder: true,
-  },
-
-  {
-    id: 2,
-    text: "Interview Appointment",
-    day: "February 13th @ 14:00pm",
-    reminder: true,
-  },
-
-  {
-    id: 3,
-    text: "Prayer Time",
-    day: "February 10th @ 16:00pm",
-    reminder: false,
-  },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+    
+    const [tasks, setTaks] = useState([
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "February 10th @ 12:00pm",
+        reminder: true,
+      },
+
+      {
+        id: 2,
+        text: "Interview Appointment",
+        day: "February 13th @ 14:00pm",
+        reminder: true,
+      },
+
+      {
+        id: 3,
+        text: "Prayer Time",
+        day: "February 10th @ 16:00pm",
+        reminder: false,
+      }
+    ]);
   return (
     <>
       {tasks.map((task) => (
